efea71c0b5b1aae7a02355574dea81337d429b48
wenogk <drf325@nyu.edu>
2021-01-15 17:51:16 +0400

    landing entries

---
+++ js/landing.js
@@ -0,0 +1,22 @@
+function loadLandingPageEntries() {
+    $("#verificationHolder").html(`
+    <center><img style="margin-top:100px;" src="assets/loader.gif" /></center>
+    `);
+  axios
+    .get("https://spacr.ml/landingEndpoint/", {
+      secret: LOGIN_SECRET
+    })
+    .then(function (response) {
+      let html = `
+      <h1 class="display-4 text-center" style="padding:10px;">${type.toUpperCase()} USERS (${
+        response.data.length
+      })</h1>`;
+      html += JSON.stringify(response.data);
+      $("#verificationHolder").html(html);
+    })
+    .catch(function (error) {
+      $("#verificationHolder").html(`
+        <div class="alert alert-dismissible alert-danger">Unauthorized. Put the correct key in.</div>
+        `);
+    });
+}
